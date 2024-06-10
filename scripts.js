3412ae855eee2177ae3bb4d93995cbc23bb29438
FusionFlow Marketing <alexgutierres235@gmail.com>
2024-06-10 07:47:40 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Añadir animaciones o funcionalidad interactiva aquí.
+});
+function toggleSidebar() {
+    var sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('show');
+};
+
